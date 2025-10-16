81ec2c35a0e4ef9fbe0eed9136a9752019e50303
Kultaran <kultaran99@proton.me>
2025-10-16 15:15:25 +0530

    Feature: Added Insertion at beginning and end of linked list

--- linked_lists.c
+++ linked_lists.c
@@ -1,1 +1,76 @@
+#include<stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 #include "linked_lists.h"
+
+typedef struct Node{
+    void* element;
+    struct Node* next;
+}node;
+
+typedef struct List{
+    struct Node* head;
+    size_t size;
+}list;
+
+list* list_create(){
+    list* l;
+    l = (list*)malloc(sizeof(list));
+    l->head == NULL;
+    l->size == 0;
+    return l;
+}
+
+void list_push_back(list* l, void* object){
+    
+    node* new_node = (node*)malloc(sizeof(node));
+    if(new_node == NULL){
+        printf("MEMORY ALLOCATION FAILED.");
+        return;
+    }
+
+    new_node->element = object;
+    new_node->next = NULL;
+
+    if(l->head == NULL){
+        l->head = new_node;
+        return;
+    }
+    node* current = l-> head;
+    while(current->next != NULL){
+        current = current->next;
+    }
+    current->next = new_node;
+    return;
+
+}
+
+void list_print(list* l){
+    if(l->head == NULL){
+        printf("List is empty! \n");
+        return;
+    }
+    node* current = l->head;
+    while(current != NULL){
+        printf("%d -->", current->element);
+        current = current->next;
+    }
+    printf("\n");
+    return;
+    
+}
+
+void list_push_front(list* l, void* object){
+
+    node* current = l->head;
+    if(current == NULL){
+        current->element = object;
+        return;
+    }
+    node* new_node = (node*)malloc(sizeof(node));
+    new_node->next = current;
+    new_node->element = object;
+    l->head = new_node;
+
+}
--- linked_lists.h
+++ linked_lists.h
@@ -1,7 +1,12 @@
 #ifndef LINKED_LISTS_H
 #define LINKED_LISTS_H
 
-struct Node node;
-void insertAtEnd(void* object, )
+typedef struct Node node;
+typedef struct List list;
+
+list* list_create();
+void list_push_back(list* l, void* object);
+void list_print(list* l);
+void list_push_front(list* l, void* object);
 
 #endif
